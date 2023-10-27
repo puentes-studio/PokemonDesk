7b8307ac792aad56e532b1f820ff3cda532a8820
Gonzalo_Rodríguez <gonzalo.jrodriguez@gmail.com>
2023-10-27 19:24:11 +0200

    POR FAVOR FUNCIONA

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,14 @@
 const URL = "https://rickandmortyapi.com/api/character";
 
 const ulElementCharacters = document.querySelector("ul")
+
+// Muestra en la consola lo que ha buscado el usuario en el formulario, al hacer click en la lupa de buscar.
+function search() {
+    let searchResult = document.getElementById("searchpokemon").value;
+    console.log(searchResult) }
+
+// Valida que haya un resultado y muestra la imagen.
+    if (searchResult) {
+        const pokemonImage = document.createElement("img");
+        pokemonImage.src = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${searchResult}.png`
+        document.body.appen }
