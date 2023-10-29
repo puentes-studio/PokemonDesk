86a059c20ce9bce79176abb6ef7f5cb74b79254e
Gonzalo_Rodríguez <gonzalo.jrodriguez@gmail.com>
2023-10-29 18:15:45 +0100

    Actualizaciones varias. Ya están todos los Pokémon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,36 @@
 'use strict';
 
-    async function getPokemonData() {
-      const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+document.getElementById("search-button").addEventListener("click", searchPokemon);
 
-      try {
-        // Fetch data from the Pokémon API
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-        const data = response.data;
-
-        // Update the HTML elements with Pokémon data
-        document.getElementById("frontImage").src = data.sprites.front_default;
-        document.getElementById("backImage").src = data.sprites.back_default;
-        document.getElementById("name").textContent = data.name;
-        document.getElementById("height").textContent = data.height / 10; // Height is in decimetres
-        document.getElementById("weight").textContent = data.weight / 10; // Weight is in hectograms
-        document.getElementById("hp").textContent = data.stats[0].base_stat;
-        document.getElementById("attack").textContent = data.stats[1].base_stat;
-        document.getElementById("defense").textContent = data.stats[2].base_stat;
-        document.getElementById("speed").textContent = data.stats[5].base_stat;
-        document.getElementById("type").textContent = data.types.map(type => type.type.name).join(", ");
-      } catch (error) {
-        console.error("Error fetching Pokémon data:", error);
-        alert("No se encontró ningún Pokémon con ese nombre.");
-      }
+function searchPokemon() {
+    const pokemonName = document.getElementById("pokemon-name").value.toLowerCase();
+    if (pokemonName.trim() === "") {
+        alert("Ingrese un nombre de Pokémon válido.");
+        return;
     }
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        .then(response => response.json())
+        .then(data => {
+            displayPokemonInfo(data);
+        })
+        .catch(error => {
+            console.error("Error al cargar los datos del Pokémon: ", error);
+            alert("No se encontró un Pokémon con ese nombre.");
+        });
+}
+
+function displayPokemonInfo(data) {
+    document.getElementById("name").textContent = data.name;
+    document.getElementById("front-image").src = data.sprites.front_default;
+    document.getElementById("back-image").src = data.sprites.back_default;
+    document.getElementById("weight").textContent = (data.weight / 10) + " kg";
+    document.getElementById("height").textContent = (data.height / 10) + " m";
+    document.getElementById("hp").textContent = data.stats[0].base_stat;
+    document.getElementById("attack").textContent = data.stats[1].base_stat;
+    document.getElementById("defense").textContent = data.stats[2].base_stat;
+    document.getElementById("speed").textContent = data.stats[5].base_stat;
+}
  
 
 
