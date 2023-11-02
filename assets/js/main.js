1b2053443aafe6c9256747ef02456465d6707640
Gonzalo_Rodríguez <gonzalo.jrodriguez@gmail.com>
2023-11-02 12:33:34 +0100

    ARREGLOS DE FALLOS

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,7 +73,7 @@ function isValidPokemonName(pokemonName) {
 }
 
 function capitalizeFirstLetter(string) {
-  string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function handleDataApi(data) {
